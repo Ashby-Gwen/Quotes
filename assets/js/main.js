360bcef2a8651cf8c4447128b3be5fbe0433b72b
Ashby-Gwen <136045238+Ashby-Gwen@users.noreply.github.com>
2024-04-05 21:34:27 +0800

    Commit 1

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+var myVar = document.getElementById("quotes-here").value; 
+console.log(myVar);
+
+function negativeQuotes() {
+    var negQuote = "Laban lang sa life, hanggang mamatay.";
+    document.getElementById("quotes-here").innerHTML = negQuote;
+    console.log(negQuote);
+}
+
+function positiveQuotes(){
+    var negQuote = "Laban lang sa Life!";
+    document.getElementById("quotes-here").innerHTML = negQuote;
+    console.log(negQuote);
+}
+
+    
